3b2aa5a45244641f5d5afb5532a65c6c3469fa77
tienttitedu <thaithanhtien1995@gmail.com>
2017-03-10 21:58:28 +0700

    fix init-model

--- db/migrate/20170310143516_create_orders.rb
+++ db/migrate/20170310143516_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
-      t.integer, :status
+      t.integer :status
       t.float :total_money
       t.references :user, foreign_key: true
 
--- db/migrate/20170310144639_create_reviews.rb
+++ db/migrate/20170310144639_create_reviews.rb
@@ -1,8 +1,8 @@
 class CreateReviews < ActiveRecord::Migration[5.0]
   def change
     create_table :reviews do |t|
-      t.string, :content
-      t.int :rate
+      t.text :content
+      t.integer :rate
       t.references :user, foreign_key: true
       t.references :product, foreign_key: true
 
